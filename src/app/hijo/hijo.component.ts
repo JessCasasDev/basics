4ca97ce2ef8c0433ac0437ca410c5e7c1ec9b515
Jessica Casas Gonzalez <jessica.casasg@perficient.com>
2022-05-27 11:11:00 -0500

    specificity, selectors, css units and responsive

---
+++ src/app/hijo/hijo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.scss'],
+})
+export class HijoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/padre/padre.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.scss'],
+})
+export class PadreComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
